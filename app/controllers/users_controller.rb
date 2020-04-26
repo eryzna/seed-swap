f3ace990c1c22b03c0ede76a2d27575049de7686
eryzna <eryzna@gmail.com>
2020-04-26 12:27:54 -0700

    users controller, routes, and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
     
+    def index
+    end
+    
+    def new
+        @user = User.new
+    end
+
+    def create
+        if (user = User.create(user_params))
+          session[:user_id] = user.id
+          redirect_to user_path(user)
+        else
+          render 'new'
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(
+          :name,
+          :password,
+          :zip_code
+        )
+    end
 end 
--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,4 +1,7 @@
 class Seed < ActiveRecord::Base
     has_many :swaps
     has_many :users, :through => :swaps
+
+    def seed_category
+    end
 end
