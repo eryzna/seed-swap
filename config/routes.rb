4f9f7ee61fa19618814961e5c80d6e937b1ffd55
eryzna <eryzna@gmail.com>
2020-04-26 21:28:50 -0700

    swap db seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup' => 'users#new'
   root 'static#home'
-  #get '/users', to:'users#index', as: 'users'
+  get '/users', to:'users#index', as: 'users'
   #post '/signup', to:'users#create'
   #get '/users/:id', to: 'users#show'
   #get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
