77835b3fb87939611a4ce1ee18dfa345e7b9bead
eryzna <eryzna@gmail.com>
2021-02-01 11:44:14 -0500

    scope method for neighbors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,100 +18,33 @@ class ApplicationController < ActionController::Base
     User.find_by(id: session[:user_id])
   end
 
-=begin IRB
-  def neighbors(user_id, seed_id)
-    @current_user= User.find_by(:id => user_id)
-    @neighbors = User.where(:zip_code => @current_user.zip_code)
-    @neighbor_swaps = @neighbors.map {|neighbor| neighbor.swaps}.flatten
-    @seed = Seed.find_by(:id => seed_id)
+  def neighbor_swaps
+    @neighbors = User.neighbors("#{current_user.zip_code}")
+    @neighbors.map {|neighbor| neighbor.swaps}.flatten
+  end
+
+  def neighbor_seed_swaps
+    @seed = Seed.find(params[:seed_id])
     @seed_swaps = @seed.swaps
     @seed_swap_ids = @seed_swaps.map {|swap| swap.id}
-    @neighbor_swap_ids = @neighbor_swaps.map {|swap| swap.id}
-    @shared_swaps = @seed_swap_ids & @neighbor_swap_ids 
-    @final_swaps = []
-    if @shared_swaps != []
-      @shared_swaps.each do |t|
-        @final_swaps << Swap.where(:id => t)
-      end
-      @final_swaps.flatten
-    else
-      puts "no swaps to show"
+    @neighbor_swap_ids = neighbor_swaps.map {|swap| swap.id}
+    @seed_swap_ids & @neighbor_swap_ids 
+  end
+
+  def require_login
+    unless session.include? :user_id
+      flash[:alert] = "You must be logged in to access that section."
+      redirect_to '/'
+      #return head(:forbidden) 
     end
   end
-=end
 
-def neighbors
-    if current_user
-       User.where(zip_code: "#{current_user.zip_code}")
+  def require_omniauth_login
+    if session.include?(:email)
+      redirect_to omniauth_new_path
     else
+      flash[:alert] = "You must be logged in to access that section."
       redirect_to '/'
     end
-    #where(zip_code: '#{current_user.zip_code}').load
   end
 end
-
-#def neighbor_swaps(zip_code)
-  #2.6.1 :032?>   neighbors = User.where(:zip_code => zip_code)
-  #2.6.1 :033?>   return neighbors.map {|user| user.swaps}.flatten
-  #2.6.1 :034?>   end
-
-def neighbor_swaps
-  neighbors.map {|neighbor| neighbor.swaps}.flatten
-end
-
-def neighbor_seed_swaps
-  @seed = Seed.find(params[:seed_id])
-  @seed_swaps = @seed.swaps
-  @seed_swap_ids = @seed_swaps.map {|swap| swap.id}
-  @neighbor_swap_ids = neighbor_swaps.map {|swap| swap.id}
-  @seed_swap_ids & @neighbor_swap_ids 
-  #@neighbor = User.where(:zip_code => zip_code)
-  #@neighbor_swaps = @neighbor.map {|neighbor| neighbor.swaps}.flatten
-  
-  #@final = []
-  #@neighbor_seed_swap.each do |t|
-    #@final << Swap.where(:id => t)
-  #end
-end
-
-def require_login
-  unless session.include? :user_id
-    flash[:alert] = "You must be logged in to access that section."
-    redirect_to '/'
-    #return head(:forbidden) 
-  end
-end
-
-def require_omniauth_login
-  if session.include?(:email)
-    redirect_to omniauth_new_path
-  else
-    flash[:alert] = "You must be logged in to access that section."
-    redirect_to '/'
-  end
-end
-
-#def neighbor_seed_swaps(seed)
-  #@seed = Seed.find_by(seed.id)
-  #@swaps = @seed.swaps
-  #@seed_swaps = @swaps.
-
-#def neighbor_seeds(zip_code)
-  #2.6.1 :049?>   neighbors = User.where(:zip_code => zip_code)
-  #2.6.1 :050?>   return neighbors.map {|neighbor| neighbor.seeds}.flatten
-  #2.6.1 :051?>   end
-
-#def neighbor_seed_swaps
-  #neighbor_swaps.where()
-  #def neighbor_seed_swaps
-   # unless session[:city_id].nil? || session[:city_id].blank?
-    #@city = City.find(session[:city_id])
-    #@deal=@city.deals
-    #@store=@deal.stores
- #end
-
- #@deals = @city.deals
-#@stores = @deals.collect(&:stores)
-
-
-  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ActiveRecord::Base
-    #validates :first_name, presence: true
-   # validates :last_name, presence: true
-   # validates :username, length: { in: 6..20}, uniqueness: true
-   ## validates :zip_code, length: { is: 5}
+    scope :neighbors, ->(zip_code) { where("zip_code == ?", zip_code) }
     has_secure_password
-
     has_many :swaps
     has_many :seeds, :through => :swaps
 
@@ -13,18 +9,16 @@ class User < ActiveRecord::Base
         @state=location[:state_name]
         @city=location[:city]
         "#{@city}, #{@state}"
-     # => {:state_code=>"GA", :state_name=>"Georgia", :city=>"Atlanta", :time_zone=>"America/New_York"}
-    #  First run will take a while, as the yaml has to be loaded
     end
-
+    
     def self.find_or_create_by_omniauth(auth_hash)
         oauth_email = auth_hash["info"]["email"]
         if @user = User.find_by(:email => oauth_email)
             return @user
         else
             @user = User.create(:email => oauth.email, :password => SecureRandom.hex)
-        
         end
     end
 
 end
+  
